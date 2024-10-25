6293b794ec6791a2cf7cd3dd1028b3215e1531be
zoninfv <zonin.fv@gmail.com>
2024-10-25 19:38:08 +0300

    Commit 25.10 19:37 test

--- Занятие 2. Условия и циклы/Условная инструкция if/Вложенные условные операторы/main.py
+++ Занятие 2. Условия и циклы/Условная инструкция if/Вложенные условные операторы/main.py
@@ -3,6 +3,7 @@ inside_condition = True
 
 if outside_condition:
     print("Условие истинно")
-    # TODO записать условный оператор, который распечатает строку
+    if inside_condition:
+        print("Ещё один условный оператор")
 else:
     print("Условие ложно")
